f6787df598259cffa664466fdb4b2b6e38e67d26
Ahmed Al-ragas <ahmedazizz703@gmail.com>
2025-06-24 22:06:52 +0300

    Update vscode.js

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function VsCode() {
   return (
     <iframe
-      src="https://github1s.com/ahmedaziz703/Ahmed-Aziz"
+      src="https://github1s.com/ahmedaziz703/Ubuntu-Ahmed"
       frameBorder="0"
       title="VsCode"
       className="h-full w-full bg-ub-cool-grey"></iframe>
