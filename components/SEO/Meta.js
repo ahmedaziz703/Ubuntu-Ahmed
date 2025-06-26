f78fca6661a9509a729d3294a39f7d2b17a2c9b1
Ahmed Al-ragas <ahmedazizz703@gmail.com>
2025-06-26 18:55:27 +0300

    Update Meta.js

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -9,16 +9,16 @@ export default function Meta() {
       <meta charSet="utf-8" />
       <meta
         name="title"
-        content="ubuntu.dawidolko.pl - Simulaing System Ubuntu"
+        content="Ahmed Al-regas - Ubuntu-Ahmed"
       />
       <meta
         name="description"
-        content="Dawid Olko's (dawidolko) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+        content="Ahmed Al-regas (AhmedAlregas) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
       />
-      <meta name="author" content="Dawid Olko (dawidolko)" />
+      <meta name="author" content="Ahmed Al-regas (AhmedAlregas)" />
       <meta
         name="keywords"
-        content="dawidolko, dawidolko's portfolio, dawidolko linux, ubuntu portfolio, dawid olko protfolio,dawid olko computer, dawid olko, dawidolko ubuntu, dawid olko ubuntu portfolio"
+        content="Ahmed Al-regas, Ahmed Al-regas portfolio, Ahmed Al-regas linux, ubuntu portfolio, Ahmed Al-regas protfolio,Ahmed Al-regas computer, Ahmed Al-regas, Ahmed Al-regas ubuntu, Ahmed Al-regas ubuntu portfolio"
       />
       <meta name="robots" content="index, follow" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
@@ -30,38 +30,38 @@ export default function Meta() {
       /* Schema.org for Google */
       <meta
         itemProp="name"
-        content="ubuntu.dawidolko.pl - Simulaing System Ubuntu"
+        content="Ahmed Al-regas - Ubuntu-Ahmed"
       />
       <meta
         itemProp="description"
-        content="dawidolko's (dawidolko) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+        content="Ahmed Al-regas (AhmedAlregas) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
       />
       <meta itemProp="image" content="images/logos/fevicon.png" />
       /* Twitter */
       <meta name="twitter:card" content="summary" />
       <meta
         name="twitter:title"
-        content="ubuntu.dawidolko.pl - Simulaing System Ubuntu"
+        content="Ahmed Al-regas - Ubuntu-Ahmed"
       />
       <meta
         name="twitter:description"
-        content="Dawid Olko's (dawidolko) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+        content="Ahmed Al-regas (AhmedAlregas) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
       />
-      <meta name="twitter:site" content="dawidolko" />
-      <meta name="twitter:creator" content="dawidolko" />
+      <meta name="twitter:site" content="AhmedAlregas" />
+      <meta name="twitter:creator" content="AhmedAlregas" />
       <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
       /* Open Graph general (Facebook, Pinterest & Google+) */
       <meta
         name="og:title"
-        content="ubuntu.dawidolko.pl - Simulaing System Ubuntu"
+        content="Ahmed Al-regas - Ubuntu-Ahmed"
       />
       <meta
         name="og:description"
-        content="Dawid Olko's (dawidolko) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+        content="Ahmed Al-regas (AhmedAlregas) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
       />
       <meta name="og:image" content="images/logos/logo_1200.png" />
-      <meta name="og:url" content="http://ubuntu.dawidolko.pl" />
-      <meta name="og:site_name" content="Dawid Olko Personal Portfolio" />
+      <meta name="og:url" content="https://ubuntu-ahmed.vercel.app" />
+      <meta name="og:site_name" content="Ahmed Al-regas Personal Portfolio" />
       <meta name="og:locale" content="en_IN" />
       <meta name="og:type" content="website" />
       <link rel="icon" href="images/logos/fevicon.svg" />
